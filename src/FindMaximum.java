40e307d3ec51fe86fc5a8164606abf7a24d22252
gaurav20619 <gauravsinghraijada619@gmail.com>
2023-03-03 12:57:35 +0530

    Add: program to find max among three String by generics.

--- src/FindMaximum.java
+++ src/FindMaximum.java
@@ -1,32 +1,29 @@
 import java.util.Scanner;
 
 public class FindMaximum {
-    public static <T> void Maximum(T num1, T num2, T num3) {
-        T max;
-        if(((Float)num1)>((Float)num2)&&((Float)num1)>((Float)num3))
-			max = num1;
-        else if (((Float)num2)>((Float)num1)&&((Float)num2)>((Float)num3)) {
-            max = num2;
-        }
+    public static <T> void Maximum(T str1, T str2, T str3) {
+		T max = str1;
+		if(((String)str2).compareTo((String)max)>0 && ((String)str2).compareTo((String)str3)>0)
+			System.out.println(str2+" is Maximum");
+		else if(((String)str3).compareTo((String)max)>0)
+			System.out.println(str3+" is Maximum");
 		else
-            max=num3;
-            System.out.println(max+" is Maximum");
-        
+			System.out.println(str1+" is Maximum");
 	}
-
+	
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
 		
 		System.out.print("Enter the First Number : ");
-		Float num1 = scan.nextFloat();
+		String str1 = scan.next();
 		
 		System.out.print("Enter the Second Number : ");
-		Float num2 = scan.nextFloat();
+		String str2 = scan.next();
 		
 		System.out.print("Enter the Third Number : ");
-		Float num3 = scan.nextFloat();
-        scan.close();
-		Maximum(num1, num2, num3);
+		String str3 = scan.next();scan.close();
+		
+		Maximum(str1, str2, str3);
 	}
 }
